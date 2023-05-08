6f968f0b5d88398962d5d48f930dceb8cc6958d9
Leandrovis22 <116530255+Leandrovis22@users.noreply.github.com>
2023-05-08 03:09:50 -0300

    Borrar exp no rompe carousel, limit 1000 char

--- miportfolioBackend1/src/main/java/com/portfolio/Leandro/Entity/Experiencia.java
+++ miportfolioBackend1/src/main/java/com/portfolio/Leandro/Entity/Experiencia.java
@@ -4,6 +4,7 @@
  */
 package com.portfolio.Leandro.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,7 @@ public class Experiencia {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombreE;
+    @Column(length = 1000)
     private String descripcionE;
 
     public Experiencia() {
--- miportfolioFrontend/src/app/components/experiencia/experiencia.component.ts
+++ miportfolioFrontend/src/app/components/experiencia/experiencia.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SlickCarouselComponent } from 'ngx-slick-carousel';
 import { Experiencia } from 'src/app/model/experiencia';
 import { SExperienciaService } from 'src/app/service/s-experiencia.service';
 import { TokenService } from 'src/app/service/token.service';
@@ -34,6 +35,7 @@ export class ExperienciaComponent implements OnInit{
     this.sExperiencia.delete(id).subscribe(
       data => {
         this.cargarExperiencia();
+        this.refreshCarousel(); // Add this line
       }, err => {
         alert("Ocurrio un error");
       }
@@ -65,6 +67,17 @@ export class ExperienciaComponent implements OnInit{
   beforeChange(e: any) {
     console.log('beforeChange');
   }
+
+
+  @ViewChild('slickModal') slickModal!: SlickCarouselComponent;
+
+  refreshCarousel(): void {
+    this.slickModal.unslick();
+    setTimeout(() => {
+      this.slickModal.initSlick();
+    }, 100);
+  }
+  
 
 }
 
