240ce9d7d9ded39a30af861cbd0cbafafa748fa6
Leandrovis22 <116530255+Leandrovis22@users.noreply.github.com>
2023-05-15 06:44:54 -0300

    ultimo 3

---
+++ miportfolioBackend1/src/main/java/com/portfolio/Leandro/Controller/HealthCheckController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.Leandro.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    
+    @GetMapping("/")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
