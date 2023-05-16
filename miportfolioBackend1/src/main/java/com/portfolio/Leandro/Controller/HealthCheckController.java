3f22fb19a3db06eac2a2a8b3a50eea0e34f49a8b
Leandrovis22 <116530255+Leandrovis22@users.noreply.github.com>
2023-05-15 22:39:21 -0300

    ultimo commit

--- miportfolioBackend1/src/main/java/com/portfolio/Leandro/Controller/HealthCheckController.java
+++ miportfolioBackend1/src/main/java/com/portfolio/Leandro/Controller/HealthCheckController.java
@@ -5,10 +5,12 @@
 package com.portfolio.Leandro.Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HealthCheckController {
     
     @GetMapping("/")
