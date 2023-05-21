72666a92bfe21e3b71f40617b65b241499268728
Leandrovis22 <116530255+Leandrovis22@users.noreply.github.com>
2023-05-20 23:44:33 -0300

    Arreglos visuales

--- miportfolioFrontend/src/app/service/constants.ts
+++ miportfolioFrontend/src/app/service/constants.ts
@@ -1,3 +1,5 @@
 //export const BASE_URL = 'http://localhost:8080/';
 
-export const BASE_URL = 'https://portfolio-backend-jih2.onrender.com/';
+//export const BASE_URL = 'https://portfolio-backend-jih2.onrender.com/';
+
+export const BASE_URL = 'https://miportfolio-production.up.railway.app/';
