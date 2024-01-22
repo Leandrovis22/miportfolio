033184bbd14cfbe6c531f81aed28c7f7f54f174e
Leandro <leandroviscolungo@gmail.com>
2024-01-22 00:22:46 -0300

    cambios mejoras

--- miportfolioFrontend/src/app/components/proyecto/proyecto.component.ts
+++ miportfolioFrontend/src/app/components/proyecto/proyecto.component.ts
@@ -25,6 +25,14 @@ export class ProyectoComponent implements OnInit {
 
   }
 
+  formatDescription(description: string): string {
+    const urlPattern = /\b((http|https):\/\/?)[\w.-]+(?:\.com)\b/gi;
+    return description.replace(urlPattern, function(url) {
+        return `<a class="project-link" href="${url}" target="_blank">${url}</a>`;
+    });
+}
+
+
   cargarProyecto(): void {
     this.sProyecto.lista().subscribe(
       data => { this.proyecto = data; })
