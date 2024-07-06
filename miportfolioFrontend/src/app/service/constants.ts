ae2a333e525718a09e8371f3bdc28222e45558df
Leandro <leandroviscolungo@gmail.com>
2024-07-06 04:15:08 -0300

    mejoras de fotos y cambio de servidor

--- miportfolioFrontend/src/app/service/constants.ts
+++ miportfolioFrontend/src/app/service/constants.ts
@@ -2,4 +2,6 @@
 
 //export const BASE_URL = 'https://portfolio-backend-jih2.onrender.com/';
 
-export const BASE_URL = 'https://miportfolio-production.up.railway.app/';
+//export const BASE_URL = 'https://miportfolio-production.up.railway.app/';
+
+export const BASE_URL = 'https://backend4-env.eba-hhskkwf6.us-east-2.elasticbeanstalk.com/';
